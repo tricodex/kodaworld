59a9fa5d5380d16a8c23018a2c48cd75d4ab680d
Patrick C <71180317+tricodex@users.noreply.github.com>
2024-07-27 14:04:41 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 import KodaWorld from '@/components/KodaWorld'
 
    export default function Home() {
-     return (
-       <div className="container mx-auto px-4">
-         <h1 className="text-4xl font-bold mb-6">Welcome to KodaWorld</h1>
-         <KodaWorld />
-       </div>
-     )
+     return <KodaWorld />
+       
+     
    }
