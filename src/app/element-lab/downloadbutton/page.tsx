818517f32a95d3f008693f2f79cfb73f5cd76bd2
Patrick C <71180317+tricodex@users.noreply.github.com>
2024-07-27 14:05:17 +0200

    Create page.tsx

---
+++ src/app/element-lab/downloadbutton/page.tsx
@@ -0,0 +1,10 @@
+import DownloadButton from '@/components/elements/DownloadButton'
+
+export default function DownloadButtonPage() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">DownloadButton Customizer</h2>
+      <DownloadButton />
+    </div>
+  )
+}
