3fc9781c9b3bf18a1752c43fc422d9516def90b9
Patrick C <71180317+tricodex@users.noreply.github.com>
2024-07-27 06:54:30 +0200

    Create page.tsx

---
+++ src/app/element-lab/button/page.tsx
@@ -0,0 +1,10 @@
+import Button from '@/components/elements/Button'
+
+export default function ButtonPage() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">Button Customizer</h2>
+      <Button />
+    </div>
+  )
+}
