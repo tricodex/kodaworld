ad00cd2d472ff8155ddff7cafbd1ef067693bc37
Patrick C <71180317+tricodex@users.noreply.github.com>
2024-07-27 14:05:10 +0200

    Create page.tsx

---
+++ src/app/element-lab/article/page.tsx
@@ -0,0 +1,10 @@
+import Article from '@/components/elements/Article'
+
+export default function ArticlePage() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">Article Customizer</h2>
+      <Article />
+    </div>
+  )
+}
