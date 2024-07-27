3c47c04f38d6ee9b928570e4c5de3a879f0138b7
Patrick C <71180317+tricodex@users.noreply.github.com>
2024-07-27 14:05:19 +0200

    Create page.tsx

---
+++ src/app/element-lab/toggle/page.tsx
@@ -0,0 +1,10 @@
+import Toggle from '@/components/elements/Toggle'
+
+export default function TogglePage() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">Toggle Customizer</h2>
+      <Toggle />
+    </div>
+  )
+}
