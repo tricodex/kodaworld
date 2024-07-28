36f6a9020140a234359472c32ea7da5db98293db
Patrick C <71180317+tricodex@users.noreply.github.com>
2024-07-29 01:49:40 +0200

    Update page.tsx

--- src/app/element-lab/button/page.tsx
+++ src/app/element-lab/button/page.tsx
@@ -3,7 +3,7 @@ import Button from '@/components/elements/Button'
 export default function ButtonPage() {
   return (
     <div>
-      <h2 className="text-2xl font-bold mb-4">Button Customizer</h2>
+      <h1 className="text-3xl font-bold mb-4">Button Customizer</h1>
       <Button />
     </div>
   )
