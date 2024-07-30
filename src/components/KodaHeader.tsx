3c32ec5862c799805d0281822776ab706a04376e
Patrick C <71180317+tricodex@users.noreply.github.com>
2024-07-30 02:04:55 +0200

    Update KodaHeader.tsx

--- src/components/KodaHeader.tsx
+++ src/components/KodaHeader.tsx
@@ -11,24 +11,25 @@ const KodaHeader: React.FC = () => {
         <Link href="/" className="text-4xl font-bold text-white animate-fade-in ml-2" style={{ fontFamily: 'var(--font-bungee-spice)', letterSpacing: '+0.0001em' }}>
           <span style={{ letterSpacing: 'inherit' }}>K</span>
           <Image 
-            src="/e1.png" 
+            src="/koda_logo128.png" 
             alt="Globe" 
-            width={55} 
-            height={40} 
+            width={40} 
+            height={20} 
             className="inline-block align-middle" 
-            style={{ marginLeft: '-0.26em', marginRight: '-0.24em' }} 
           />
           <span style={{ letterSpacing: 'inherit' }}>DA</span>
         </Link>
         <div className="flex items-center space-x-4">
-        <Link href="/element-lab" passHref>
+          <Link href="/element-lab" passHref>
             <Button variant="ghost" className="text-black hover:bg-black/20">Element Lab</Button>
+          </Link>
+          <Link href="/koda-survival" passHref>
+            <Button variant="ghost" className="text-black hover:bg-black/20">Koda Survival</Button>
           </Link>
           <Button variant="ghost" size="icon" aria-label="Notifications" className="text-black hover:bg-black/20"><Bell className="h-5 w-5" /></Button>
           <Button variant="ghost" size="icon" aria-label="Settings" className="text-black hover:bg-black/20"><Settings className="h-5 w-5" /></Button>
           <Button variant="ghost" size="icon" aria-label="Login" className="text-black hover:bg-black/20"><LogIn className="h-5 w-5" /></Button>
           <Button variant="ghost" className="hidden sm:inline-flex text-black border-white hover:bg-black/20">Upgrade Plan</Button>
-          
         </div>
       </div>
     </header>
