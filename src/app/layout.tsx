b420e39186a1b753e4f548b245713dab9089f5ea
Patrick C <71180317+tricodex@users.noreply.github.com>
2024-08-03 03:51:06 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Bungee_Spice, Londrina_Shadow } from "next/font/google";
 import "./globals.css";
 import "../styles/animalButtons.css";
+// import koda surv
 import Link from 'next/link';
 
 const inter = Inter({ subsets: ["latin"] });
